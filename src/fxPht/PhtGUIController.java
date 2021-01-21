cf54e922a983f9878a982d5263e74f6880fae4ff
Puuppo Joonas Antti Juhana <joonas.a.j.puuppo@student.jyu.fi>
2021-01-21 15:55:31 +0200

    Tehty Eclipse-projekti ohjeiden mukaan

---
+++ src/fxPht/PhtGUIController.java
@@ -0,0 +1,10 @@
+package fxPht;
+
+/**
+ * @author Joonas Puuppo
+ * @version Jan 21, 2021
+ *
+ */
+public class PhtGUIController {
+	//
+}
---
+++ src/fxPht/PhtMain.java
@@ -0,0 +1,35 @@
+package fxPht;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+
+
+/**
+ * @author joona
+ * @version Jan 21, 2021
+ *
+ */
+public class PhtMain extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("PhtGUIView.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("pht.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * @param args ei käytössä
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
