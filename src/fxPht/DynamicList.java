c8e624be757af08e2f1e22f7de8bcf7a17afac88
Valtteri Rajalainen <rajalainen.valtteri@gmail.com>
2021-02-18 20:04:59 +0200

    Lisätty append- ja pop-metodit listaan

--- src/fxPht/DynamicList.java
+++ src/fxPht/DynamicList.java
@@ -50,33 +50,53 @@ public class DynamicList<T> {
      * @param item Item to be appended to the list.
      */
     public void append(T item) {
-        //
+        data[count] = item;
+        count += 1;
+        if (expansionNeeded()) {
+            expand();
+        }
+    }
+    
+    
+    /**
+     * @param index Index to access the item. If index is negative,
+     * count is started from the end of the list. 
+     * For example index "-1" refers to the last item of the list.
+     * @return The item in the index.
+     * @throws IndexOutOfBoundsException -
+     */
+    public T get(int index) throws IndexOutOfBoundsException{
+        if (index < 0) {
+            return getByNegativeIndex(index);
+        }
+        if (index > count - 1) {
+            throw new IndexOutOfBoundsException();
+        }
+        return data[index];
     }
     
     
     /**
      * @param index --
      * @return --
+     * @throws IndexOutOfBoundsException -
      */
-    public T get(int index) {
-        return null;
-    }
-    
-    
-    /**
-     * @param index --
-     * @return --
-     */
-    public T pop(int index) {
-        return null;
+    public T pop(int index) throws IndexOutOfBoundsException{
+        T item = data[index];
+        if (shrinkNeeded()) {
+            shrink(index);
+        } else {
+            repackItems(index);
+        }
+        count -= 1;
+        return item;
     }
     
     
     /**
      * @param item --
-     * @param index --
      */
-    public void insert(T item, int index) {
+    public void remove(T item) {
         //
     }
     
@@ -94,27 +114,51 @@ public class DynamicList<T> {
     }
     
     
-    @SuppressWarnings("unused")
     private boolean expansionNeeded() {
-        return false;
+        return (double)count / (double)size > EXPAND_TRESHOLD;
     }
     
     
-    @SuppressWarnings("unused")
     private boolean shrinkNeeded() {
-        return false;
+        boolean result = false;
+        if ((double)count / (double)size < SHRINK_TRESHOLD && size > START_SIZE) {
+            result = true;
+        }
+        return result;
     }
     
     
-    @SuppressWarnings("unused")
+    @SuppressWarnings("unchecked")
     private void expand() {
-        //
+        int newSize = size * 2;
+        T[] temp = (T[])new Object[newSize];
+        for (int i = 0; i < count; i++) {
+            temp[i] = data[i];
+        }
+        size = newSize;
+        data = temp;
     }
     
     
-    @SuppressWarnings("unused")
-    private void shrink() {
-        //
+    @SuppressWarnings("unchecked")
+    private void shrink(int index) {
+        int newSize = size / 2;
+        T[] temp = (T[])new Object[newSize];
+        for (int i = 0; i < index; i++) {
+            temp[i] = data[i];
+        }
+        for (int j = index; j < count; j++) {
+            temp[j] = data[j+1];
+        }
+        size = newSize;
+        data = temp;
+    }
+    
+    
+    private void repackItems(int index) {
+        for (int i = index; i < count; i++) {
+            data[i] = data[i+1];
+        }
     }
     
     
@@ -122,5 +166,13 @@ public class DynamicList<T> {
         for (int i = 0; i < items.length; i++) {
             data[i] = items[i];
         }
+    }
+    
+    
+    private T getByNegativeIndex(int index) throws IndexOutOfBoundsException {
+        if (Math.abs(index) > count) {
+            throw new IndexOutOfBoundsException();
+        }
+        return data[count + index];
     }
 }
