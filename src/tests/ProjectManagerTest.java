be39b1e5c31fe39e66eff6b5deada953b4714160
Valtteri Rajalainen <rajalainen.valtteri@gmail.com>
2021-02-19 14:01:49 +0200

    Luotiin pohjat Project ja ProjectManager-luokille

---
+++ src/tests/ProjectManagerTest.java
@@ -0,0 +1,32 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+
+import fxPht.ProjectManager;
+
+/**
+ * @author varajala
+ * @version Feb 19, 2021
+ */
+public class ProjectManagerTest {
+    
+    /**
+     * Assert that only one instance can be created.
+     */
+    @Test
+    public void testGettingInstance() {
+        ProjectManager ins1 = ProjectManager.getInstance();
+        ProjectManager ins2 = ProjectManager.getInstance();
+        assertTrue(ins1 == ins2);
+        
+        try {
+            ProjectManager pm = new ProjectManager();
+            System.out.println(pm);
+            fail();
+        } catch (Exception e) {
+            assertTrue(true);
+        }
+    }
+}
---
+++ src/tests/ProjectTest.java
@@ -0,0 +1,16 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+
+import fxPht.Project;
+
+
+/**
+ * @author varajala
+ * @version Feb 19, 2021
+ */
+public class ProjectTest {
+    // TODO
+}
