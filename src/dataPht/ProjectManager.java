3d1709fab21cd19ca15b765ee310a1af34dff1df
Valtteri Rajalainen <rajalainen.valtteri@gmail.com>
2021-02-23 13:10:11 +0200

    Lisättiin tarkistus Projektin nimelle

--- src/dataPht/ProjectManager.java
+++ src/dataPht/ProjectManager.java
@@ -1,5 +1,8 @@
 package dataPht;
 
+import java.util.regex.*;
+
+
 /**
  * @author varajala
  * @version Feb 19, 2021
@@ -73,6 +76,75 @@ public class ProjectManager {
     public Project createNewProject(String name) {
         return null;
         // TODO
+    }
+    
+    
+    /**
+     * Check if the given String is valid for Project's name.
+     * This is basically checking if the String can be used as
+     * a filename in Windows, Mac and Linux.
+     * 
+     * Checks:
+     * - if String is blank
+     * - if String starts or ends with whitespace
+     * - if String ends with a dot
+     * - if String contains restricted characters
+     * - if String contains reserved names
+     * 
+     * @param name Name of the Project
+     * @return boolean
+     */
+    public boolean isValidProjectName(String name) {
+        if (name.isBlank())                     return false;
+        if (startsWithWhitespace(name))         return false;
+        if (endsWithWhitespace(name))           return false;
+        if (endsWithDot(name))                  return false;
+        if (containsReservedCharacters(name))   return false;
+        if (containsReservedNames(name))        return false;
+        return true;
+    }
+
+
+    private boolean endsWithDot(String name) {
+        return name.endsWith(".");
+    }
+
+
+    private boolean containsReservedNames(String name) {
+        final String[] RESERVED_NAMES = {
+                "CON", "PRN", "AUX", "NUL",
+                "COM1", "COM2", "COM3", "COM4",
+                "COM5", "COM6", "COM7", "COM8",
+                "COM9", "LPT1", "LPT2", "LPT3",
+                "LPT4", "LPT5", "LPT6", "LPT7",
+                "LPT8", "LPT9"
+        };
+        for (String reservedName : RESERVED_NAMES) {
+            Pattern pattern = Pattern.compile(reservedName);
+            Matcher matcher = pattern.matcher(name);
+            if (matcher.find()) return true;
+        }
+        return false;
+    }
+
+
+    private boolean containsReservedCharacters(String name) {
+        final String RESTRICTED_CHARS = "<|>|:|\"|/|\\||\\?|\\*|-|\\\\";
+        Pattern pattern = Pattern.compile(RESTRICTED_CHARS);
+        Matcher matcher = pattern.matcher(name);
+        return matcher.find();
+    }
+
+
+    private boolean endsWithWhitespace(String name) {
+        char last = name.charAt(name.length()-1);
+        return Character.isWhitespace(last);
+    }
+
+
+    private boolean startsWithWhitespace(String name) {
+        char first = name.charAt(0);
+        return Character.isWhitespace(first);
     }
     
 }
