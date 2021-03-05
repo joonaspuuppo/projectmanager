9ddff58e26c22d0e134f0c5178dfde55404e28f9
Puuppo Joonas Antti Juhana <joonas.a.j.puuppo@student.jyu.fi>
2021-03-05 13:27:18 +0200

    Lisätty oletusarvot Taskin konstruktoriin.

--- src/dataPht/Task.java
+++ src/dataPht/Task.java
@@ -16,6 +16,10 @@ public class Task {
      */
     public Task(int id) {
         this.id = id;
+        name = "Uusi tehtävä " + id;
+        info = "";
+        done = false;
+        priority = Priority.MEDIUM;
     }
     
     /**
